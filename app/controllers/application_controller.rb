05aea213f187f98b112331e27da53494d12260fb
Hansel López <halm29799@gmail.com>
2022-11-02 22:40:46 -0600

    feat: finish devise authentication and add home page
    > rails g controller Home
    > rails db:create
    > rails db:create RAILS_ENV=test
    > rake db:migrate
    > rake db:migrate RAILS_ENV=test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource_or_scope)
+    # Redirect to home page after sign in
+    root_path
+  end
+
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :current_password])
+    end
 end
