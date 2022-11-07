96f0b05e546c5a0d4649f88377c22cdd271971ea
Hansel López <halm29799@gmail.com>
2022-11-07 13:30:31 -0600

    feat: create URL taxonomy and preview as public

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,13 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Create route for /subdomain/ to redirect to /subdomain/blogs/:blog_id
+  get '/:subdomain', to: 'blogs#show', id: 1, as: 'subdomain'
+
+  # Create route for /subdomain/subtopic to redirect to /subdomain/subtopic/1
+  get '/:subdomain/:subtopic', to: 'pages#show', id: 1, as: 'subdomain_page'
+
   # Defines the root path route ("/")
   root "home#index"
+
 end
