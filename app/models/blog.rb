ee36b8edb171f3f2f5d162ef1471c15ad81083f6
Hansel López <halm29799@gmail.com>
2022-11-07 07:46:00 -0600

    feat: add style to blog's views and check behavior

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,16 +2,18 @@ class Blog < ApplicationRecord
   # Associations
   has_and_belongs_to_many :users, join_table: :teams, dependent: :destroy
 
+  has_one_attached :image
+
   # Before save replace every space in name with a dash
   before_save :replace_spaces_with_dashes
 
   # Validations
   validates :name, presence: true, uniqueness: true
-  validates :subdomain, presence: true, uniqueness: true
+  validates :subdomain, uniqueness: true
 
 
-  # Replace every space in name with a dash
+  # Replace every space in name with a dash and delete dash at the end and beginning of the string
   def replace_spaces_with_dashes
-    self.subdomain = self.name.gsub(/\s+/, "-")
+    self.subdomain = self.name.downcase.gsub(" ", "-").gsub(/^-/, "").gsub(/-$/, "")
   end
 end
